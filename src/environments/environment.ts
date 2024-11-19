c272bf88466f5d244c69acb77541c93e9f904b2d
Miguelmlj <hsmiguelhs@gmail.com>
2024-11-18 17:42:37 -0700

    handling aws server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://actas-sockets-backend-production.up.railway.app',
+    // apiUrl: 'https://actas-sockets-backend-production.up.railway.app',
+    apiUrl: 'http://52.11.7.156:3000',
     // apiUrl: 'http://localhost:3000',
     // apiUrl: 'http://192.168.100.41:3000',
     secretKey: 'TU_CLAVE_SECRETA',
