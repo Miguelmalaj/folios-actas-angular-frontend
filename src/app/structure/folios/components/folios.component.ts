d89b42d420ac073f45150db5147251062d7de812
MiguelAdroituser <miguel@adroitoverseas.com>
2025-11-20 22:01:52 -0700

    Feat - Queretaro seal.

--- src/app/structure/folios/components/folios.component.ts
+++ src/app/structure/folios/components/folios.component.ts
@@ -754,7 +754,7 @@ export class FoliosComponent implements OnInit, OnDestroy {
         NUEVOLEON: { x: width - sealDims.width - 335, y: 90 }, // bottom-right
         OAXACA: { x: width - sealDims.width - 335, y: 90 }, // bottom-right
         PUEBLA: { x: width - sealDims.width - 337, y: 90 }, // bottom-right
-        QUERETARO: { x: width - sealDims.width - 330, y: 90 }, // bottom-right
+        QUERETARO: { x: width - sealDims.width - 330, y: 100 }, // bottom-right
         QUINTANAROO: { x: width - sealDims.width - 336, y: 103 }, // bottom-right
         SANLUISPOTOSI: { x: width - sealDims.width - 322, y: 90 }, // bottom-right
         SINALOA: { x: width - sealDims.width - 342, y: 90 }, // bottom-right
